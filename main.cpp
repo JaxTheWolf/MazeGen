5de48c2ab0f34387197569cead1fe4bf630ca19b
Roman Lubij <roman.lubij@gmail.com>
2025-04-07 18:40:22 +0200

    Add old code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,103 @@
+#include <cstdlib>
 #include <iostream>
+#include <vector>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+#define endl '\n'
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+struct wallsOrPath {
+  bool top;
+  bool bottom;
+  bool left;
+  bool right;
+};
+
+class Cell {
+ public:
+  bool visited;
+  bool solution;
+  unsigned int x, y;
+  wallsOrPath walls;
+  wallsOrPath path;
+
+  static auto findCell(const std::vector<Cell> &grid, const unsigned int x, const unsigned int y) {
+    const unsigned long index = grid.size() - (x + y) - 1;
+    if (grid.size() >= x + y) return grid[index];
+    else return grid[x+y];
+  }
+
+  [[nodiscard]] auto checkNeighbors(std::vector<Cell> grid) const {
+    std::vector<Cell> neighbors;
+    Cell top{}, bottom{}, left{}, right{};
+
+    top = Cell::findCell(grid, this->x, this->y - 1);
+    bottom = Cell::findCell(grid, this->x - 1, this->y);
+    left = Cell::findCell(grid, this->x, this->y + 1);
+    right = Cell::findCell(grid, this->x + 1, this->y);
+
+    if (!top.visited && top.x != this->x) neighbors.push_back(top);
+    if (!bottom.visited && bottom.x != this->x) neighbors.push_back(bottom);
+    if (!left.visited && bottom.x != this->x) neighbors.push_back(left);
+    if (!right.visited && bottom.x != this->x) neighbors.push_back(right);
+
+    return grid[std::rand() % neighbors.size() - 1];
+  }
+};
+
+class Maze {
+ private:
+  int xSize = 0, ySize = 0;
+
+ public:
+  std::vector<Cell> grid;
+};
+
+unsigned int maze_x, maze_y, randSeed = 0;
+
+int main(int argc, char* argv[]) {
+  // Populace velikosti bludiště
+  std::cout << "Zadejte šířku bludiště: ";
+  std::cin >> maze_x;
+  std::cout << "Zadejte výšku bludiště: ";
+  std::cin >> maze_y;
+  std::cout << "Zadejte seed: ";
+  std::cin >> randSeed;
+
+  std::srand(randSeed);
+
+  Maze maze;
+
+  for (unsigned int i = 0; i > maze_y; i++) {
+    for (unsigned int j = 0; j > maze_x; j++) {
+      Cell cell{};
+      cell.x = i - 1;
+      cell.y = j - 1;
+      cell.walls.top = cell.walls.bottom = cell.walls.left = cell.walls.right =
+          true;
+      cell.path.top = cell.path.bottom = cell.path.left = cell.path.right =
+          false;
+
+      if (i == 1 && j == 2) {
+        cell.solution = true;
+        cell.visited = true;
+      } else {
+        cell.solution = cell.visited = false;
+      }
+      maze.grid.push_back(cell);
+    }
+  }
+
+ // if (argv[1]) {
+    for (size_t index = 0; index < maze.grid.size(); ++index) {
+      const Cell currentCell = maze.grid[index];
+      std::cout << "i: " << index;
+      std::cout << " X: " << currentCell.x << " Y: " << currentCell.y;
+      std::cout << " Sol: " << currentCell.solution
+                << " Vis: " << currentCell.visited << '\n';
     }
 
-    return 0;
-}
+    const Cell randCell = maze.grid[0].checkNeighbors(maze.grid);
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+    std::cout << Cell::findCell(maze.grid, 0, 1).visited << endl;
+    std::cout << randCell.x << " " << randCell.y << endl;
+//  }
+}
